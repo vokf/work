af5c4fb0fe6ef29953140645115d3a5be56fe4d6
demo <2297802467@qq.com>
2020-05-01 19:44:04 +0800

    UserController.java

--- src/main/java/cn/edu/svtcc/controller/UserController.java
+++ src/main/java/cn/edu/svtcc/controller/UserController.java
@@ -6,11 +6,13 @@ import cn.edu.svtcc.service.UserService;
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@CrossOrigin
 @Controller
 public class UserController {
   @Autowired
