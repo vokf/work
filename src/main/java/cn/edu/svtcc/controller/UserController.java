bf93cf3e47fae676ceb63d3480fab1c560f32f59
demo <2297802467@qq.com>
2020-05-05 13:39:38 +0800

    UserController.java

--- src/main/java/cn/edu/svtcc/controller/UserController.java
+++ src/main/java/cn/edu/svtcc/controller/UserController.java
@@ -6,30 +6,49 @@ import cn.edu.svtcc.service.UserService;
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author demo
+ */
 @CrossOrigin
 @Controller
+@SessionAttributes(types = Users.class, value = "user")
+@Transactional(rollbackFor = Exception.class)
 public class UserController {
+
+  private UserService service;
+
   @Autowired
-  UserService service;
+  public void setService(UserService service) {
+    this.service = service;
+  }
 
   @ResponseBody
-  @RequestMapping(value = "/login",method = RequestMethod.POST)
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+
   public ResultBean login(@RequestBody(required = true) String json) {
     Users users = (Users) JSON.parseObject(json, Users.class);
-    System.out.println(users);
+
     Users getUser = this.service.login(users);
+
     return getUser != null ? ResultBean.success(getUser) : ResultBean.fail();
 
   }
 
   @ResponseBody
-  @RequestMapping(value = "/register",method = RequestMethod.POST)
+  @RequestMapping(value = "/register", method = RequestMethod.POST)
   public ResultBean register(@RequestBody String json) {
     Users users = (Users) JSON.parseObject(json, Users.class);
 
@@ -39,6 +58,7 @@ public class UserController {
 
   @RequestMapping("/index")
   public String index() {
+
 
     return "userLogin.html";
   }
