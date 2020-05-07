684c8c5942b5c7e8816a0711d0bc1a1184416c66
demo <2297802467@qq.com>
2020-05-07 15:53:00 +0800

    UserController.java

--- src/main/java/cn/edu/svtcc/controller/UserController.java
+++ src/main/java/cn/edu/svtcc/controller/UserController.java
@@ -5,6 +5,7 @@ import cn.edu.svtcc.pojo.Users;
 import cn.edu.svtcc.service.UserService;
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -24,12 +25,12 @@ import javax.servlet.http.HttpSession;
  */
 @CrossOrigin
 @Controller
-@SessionAttributes(types = Users.class, value = "user")
+@SessionAttributes(types = ResultBean.class)
 @Transactional(rollbackFor = Exception.class)
 public class UserController {
 
-  private UserService service;
 
+  private UserService service;
   @Autowired
   public void setService(UserService service) {
     this.service = service;
@@ -37,16 +38,19 @@ public class UserController {
 
   @ResponseBody
   @RequestMapping(value = "/login", method = RequestMethod.POST)
-
-  public ResultBean login(@RequestBody(required = true) String json) {
+  public ResultBean login(@NonNull@RequestBody(required = true) String json,@NonNull HttpServletRequest request) {
+    //将从页面传来的json转换成users
     Users users = (Users) JSON.parseObject(json, Users.class);
-
+    //调用service层的login方法
     Users getUser = this.service.login(users);
+    //通过resultBean中的success方法 或者 fail 方法 给前端响应
+    ResultBean bean = getUser != null ? ResultBean.success(getUser) : ResultBean.fail();
+    //将用户的登录信息保存在session中
+    request.getSession().setAttribute("user_login",bean);
 
-    return getUser != null ? ResultBean.success(getUser) : ResultBean.fail();
+    return bean ;
 
   }
-
   @ResponseBody
   @RequestMapping(value = "/register", method = RequestMethod.POST)
   public ResultBean register(@RequestBody String json) {
@@ -57,9 +61,10 @@ public class UserController {
   }
 
   @RequestMapping("/index")
-  public String index() {
+  public String index( @NonNull HttpServletRequest request) {
 
-
+    Object user_login = request.getSession().getAttribute("user_login");
+    System.out.println(user_login);
     return "userLogin.html";
   }
 }
