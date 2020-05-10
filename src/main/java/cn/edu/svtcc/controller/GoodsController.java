e2de61ce8697ff6627a735502f26b84c9cf8db09
demo <2297802467@qq.com>
2020-05-10 22:12:06 +0800

    GoodsController.java

---
+++ src/main/java/cn/edu/svtcc/controller/GoodsController.java
@@ -0,0 +1,16 @@
+package cn.edu.svtcc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author demo
+ * @title GoodsController
+ * @description
+ * @date 2020/05/10/21:00
+ */
+
+@Controller
+public class GoodsController {
+
+
+}
