8d7508413aa1e97990e3e9b995f38fb1be27a830
demo <2297802467@qq.com>
2020-05-10 21:01:52 +0800

    Goods.java

---
+++ src/main/java/cn/edu/svtcc/pojo/Goods.java
@@ -0,0 +1,28 @@
+package cn.edu.svtcc.pojo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author demo
+ * @title Goods
+ * @description
+ * @date 2020/05/10/20:39
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Goods implements Serializable {
+
+  private Integer id;
+  private String goodName;
+  private Integer goodPrice;
+  private String goodImage;
+  private String hotGood;
+
+}
