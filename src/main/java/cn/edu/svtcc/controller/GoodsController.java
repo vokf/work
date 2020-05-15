340112ef5b428af05827287be1909c601eb0f09e
demo <2297802467@qq.com>
2020-05-15 09:45:52 +0800

    GoodsController.java

--- src/main/java/cn/edu/svtcc/controller/GoodsController.java
+++ src/main/java/cn/edu/svtcc/controller/GoodsController.java
@@ -1,6 +1,18 @@
 package cn.edu.svtcc.controller;
 
+import cn.edu.svtcc.pojo.Goods;
+import cn.edu.svtcc.service.GoodService;
+import cn.edu.svtcc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * @author demo
@@ -13,4 +25,32 @@ import org.springframework.stereotype.Controller;
 public class GoodsController {
 
 
+  private GoodService service;
+
+  @Autowired
+  public void setService(GoodService service) {
+    this.service = service;
+  }
+
+
+  @RequestMapping("/hh")
+  public String hh(@NonNull Model model) {
+    model.addAttribute("demo", service.selectAllGoods());
+
+    return "hh";
+  }
+
+  @RequestMapping("/findAllGoods")
+  @ResponseBody
+  public List<Goods> findAllGoods() {
+    return this.service.selectAllGoods();
+  }
+
+  @RequestMapping("/index1")
+  public String index1(@NonNull Model model) {
+    model.addAttribute("abc", service.selectAllGoods());
+    model.addAttribute("hot", service.selectGoods());
+
+    return "index";
+  }
 }
